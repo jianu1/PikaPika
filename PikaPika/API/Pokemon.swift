bc6a961e092be5b8bd6b77fbe605a15b4227c325
Marius Stefanita Jianu <jianu.stefa@yahoo.com>
2023-04-28 23:10:46 +0300

    delete files that are no longer needed

--- PikaPika/API/Pokemon.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Pokemon.swift
-//  PikaPika
-//
-//  Created by Marius Stefanita Jianu on 28.04.2023.
-//
-// https://pokeapi.co/api/v2/pokemon/
-
-import Foundation
--- PikaPika/API/PokemonSelected.swift
+++
@@ -1,8 +0,0 @@
-//
-//  PokemonSelected.swift
-//  PikaPika
-//
-//  Created by Marius Stefanita Jianu on 28.04.2023.
-//
-
-import Foundation
